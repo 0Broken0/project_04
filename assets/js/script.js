ca1620f855539020f148391185e9ad63a7c08d63
Broken100 <luxo_pjs@hotmail.com>
2024-02-14 14:50:15 -0500

    project_04

---
+++ assets/js/script.js
@@ -0,0 +1,47 @@
+'use strict';
+
+
+
+// add Event on multiple element
+
+const addEventOnElements = function (elements, eventType, callback) {
+  for (let i = 0; i < elements.length; i++) {
+    elements[i].addEventListener(eventType, callback);
+  }
+}
+
+
+
+// PRELOADING
+
+const loadingElement = document.querySelector("[data-loading]");
+
+window.addEventListener("load", function () {
+  loadingElement.classList.add("loaded");
+  document.body.classList.remove("active");
+});
+
+//MOBILE NAV TOGGLE
+
+const [navTogglers, navLinks, navbar, overlay] = [
+  document.querySelectorAll("[data-nav-toggler]"),
+  document.querySelectorAll("[data-nav-link]"),
+  document.querySelectorAll("[data-nav-bar]"),
+  document.querySelectorAll("[data-overlay]"),
+];
+
+const toggleNav = function () {
+  navbar.classList.toggle("active");
+  overlay.classList.toggle("active");
+  document.body.classList.toggle("active");
+}
+
+addEventOnElements(navTogglers, "click", toggleNav);
+
+const closeNav = function () {
+  navbar.classList.remove("active");
+  overlay.classList.remove("active");
+  document.body.classList.remove("active");
+}
+
+addEventOnElements(navLinks, "click", closeNav);
